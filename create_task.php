4812619c2c02162595ce5ebaefea9867535c7487
AlbertJin1 <awsomegamingracerhd3@gmail.com>
2024-02-26 10:42:36 +0800

    CREATE AND UPDATE CONPLETE

---
+++ create_task.php
@@ -0,0 +1,93 @@
+<?php session_start();
+include("config.php");
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+    <h1 class="text-center mt-4">Creating a Task</h1>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-lg-9">
+                <form action="process.php" method="POST">
+                    <div class="row">
+                        <div class="col-md-12 mb-3">
+                            <label for="studentId" class="form-label">Title</label>
+                            <input type="text" class="form-control" id="title" name="title">
+                        </div>
+
+                        <div class="col-md-12 mb-3">
+                            <label for="firstName" class="form-label">Description</label>
+                            <textarea id="textarea" name="description" rows="5" cols="30"
+                                class="form-control"></textarea>
+                        </div>
+
+                        <div class="col-md-4 mb-3">
+                            <label for="middlename" class="form-label">Priority</label>
+                            <select id="priority" name="priority" class="form-control">
+                                <option value="" disabled selected>Select priority</option>
+                                <option value="Low">Low</option>
+                                <option value="Medium">Medium</option>
+                                <option value="High">High</option>
+                            </select>
+                        </div>
+
+                        <div class="col-md-4 mb-3">
+                            <label for="dateOfBirth" class="form-label">Due Date</label>
+                            <input type="date" class="form-control" id="dueDate" name="due_date">
+                        </div>
+
+                        <div class="col-md-12 mb-3 text-center mt-4">
+                            <button type="button" value="Back" class="btn btn-primary" onclick="history.back();"
+                                style="float: left;">Go Back</button>
+                            <button type="submit" name="insertTask" class="btn btn-primary "
+                                style="float: right;">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <?php
+    if (isset($_SESSION['status']) && $_SESSION['status_code'] != '') {
+        ?>
+        <script>
+            swal({
+                title: "<?php echo $_SESSION['status']; ?>",
+                icon: "<?php echo $_SESSION['status_code']; ?>",
+            });
+        </script>
+        <?php
+        unset($_SESSION['status']);
+        unset($_SESSION['status_code']);
+    }
+    ?>
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+include("config.php");
+
+// PROCESS START ------------------------
+if (isset($_POST["loginButton"])) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $login_query = "SELECT `id`, `email`, `password`, `fname`, `mname`, `lname` FROM `user` WHERE `email` = '$email' AND `password` = '$password' LIMIT 1 ";
+    $login_result = mysqli_query($con, $login_query);
+
+    if (mysqli_num_rows($login_result) == 1) {
+        $_SESSION['status'] = "Welcome!";
+        $_SESSION['status_code'] = "success";
+        header("Location: index.php");
+        exit();
+    } else {
+        $_SESSION['status'] = "Invalid Username/Password";
+        $_SESSION['status_code'] = "error";
+        header("Location: login.php");
+        exit();
+    }
+}
+
+if (isset($_POST["insertTask"])) {
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $priority = $_POST['priority'];
+    $due_date = $_POST['due_date'];
+
+    $check_task_duplicate_query = "SELECT * FROM `tasks` WHERE `title` = '$title' AND `description` = '$description' AND `priority` = '$priority' AND `due_date` = '$due_date'";
+    $task_duplicate_result = mysqli_query($con, $check_task_duplicate_query);
+    $task_duplicate_count = mysqli_num_rows($task_duplicate_result);
+
+    if ($task_duplicate_count > 0) {
+        $_SESSION['status'] = "Task Already Exists! Please try again.";
+        $_SESSION['status_code'] = "error";
+        header("Location: insert.php");
+        exit();
+    }
+
+    $query = "INSERT INTO `tasks`(`title`, `description`, `priority`, `due_date`) VALUES ('$title','$description','$priority','$due_date')";
+    $query_result = mysqli_query($con, $query);
+
+    if ($query_result) {
+        $_SESSION['status'] = "Task Added!";
+        $_SESSION['status_code'] = "success";
+        header("Location: index.php");
+        exit();
+    }else {
+        $_SESSION['status'] = "Something went wrong! Please try again.";
+        $_SESSION['status_code'] = "error";
+        header("Location: insert.php");
+        exit();
+    }
+
+}
+
+if (isset($_POST["updateTask"])) {
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $priority = $_POST['priority'];
+    $due_date = $_POST['due_date'];
+
+    $query = "UPDATE `tasks` SET `title`='$title',`description`='$description',`priority`='$priority',`due_date`='$due_date' WHERE 1";
+    $query_result = mysqli_query($con, $query);
+
+    if ($query_result) {
+        $_SESSION['status'] = "Task Updated!";
+        $_SESSION['status_code'] = "success";
+        header("Location: index.php");
+        exit();
+    }else {
+        $_SESSION['status'] = "Something went wrong! Please try again.";
+        $_SESSION['status_code'] = "error";
+        header("Location: insert.php");
+        exit();
+    }
+
+}
+
+?>
