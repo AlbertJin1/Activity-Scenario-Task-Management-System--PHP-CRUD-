3d230bbe216c6a325f886be7374f8dbe5bf7a709
AlbertJin1 <awsomegamingracerhd3@gmail.com>
2024-02-28 22:37:10 +0800

    FINAL

--- index.php
+++ index.php
@@ -21,11 +21,87 @@ include("config.php");
 
 <body>
 
-    <?php
+    <div class="container-fluid mt-4">
+        <section class="section">
+            <div class="row">
+                <div class="col-lg-12">
 
-    include 'view_tasks.php';
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Task Management System</h5>
 
-    ?>
+                            <a href="create_task.php" style="float: right;" class="btn task" id="mgaButtons">Add a
+                                Task</a>
+
+                            <!-- Table with stripped rows -->
+                            <table class="table datatable">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Priority</th>
+                                        <th scope="col">Due Date</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+
+                                    <?php
+                                    $query = "SELECT * FROM `tasks`";
+                                    $query_run = mysqli_query($con, $query);
+                                    if (mysqli_num_rows($query_run) > 0) {
+                                        foreach ($query_run as $row) {
+                                            ?>
+                                            <tr>
+                                                <td>
+                                                    <?= $row['title']; ?>
+                                                </td>
+                                                <td>
+                                                    <?= $row['description']; ?>
+                                                </td>
+                                                <td>
+                                                    <?= $row['priority']; ?>
+                                                </td>
+                                                <td>
+                                                    <?= $row['due_date']; ?>
+                                                </td>
+
+                                                <td class="text-center vstack gap-3 mx-auto">
+
+                                                    <a type="button" class="btn btn-primary" id="customFont-Button"
+                                                        href="view_tasks.php?id=<?= $row['id']; ?>">VIEW</a>
+                                                    <a type="button" class="btn btn-warning" id="customFont-Button"
+                                                        href="edit_task.php?id=<?= $row['id']; ?>">UPDATE</a>
+                                                    <a type="button" class="btn btn-danger" id="customFont-Button"
+                                                        href="delete_task.php?id=<?= $row['id']; ?>">DELETE</a>
+
+                                                </td>
+                                            </tr>
+
+                                            <?php
+                                        }
+                                    } else {
+                                        ?>
+                                        <tr>
+                                            <td colspan="6">No Record Found</td>
+                                        </tr>
+                                        <?php
+                                    }
+                                    ?>
+
+                                </tbody>
+                            </table>
+                            <!-- End Table with stripped rows -->
+                            <footer>
+                                <p>Task Management System | Regualos & Sarmiento @ 2024</p>
+                            </footer>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
--- view_tasks.php
+++ view_tasks.php
@@ -1,80 +1,116 @@
-<div class="container-fluid mt-4">
-    <section class="section">
-        <div class="row">
-            <div class="col-lg-12">
+<?php session_start();
+include("config.php");
+?>
+<!doctype html>
+<html lang="en">
 
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Task Management System</h5>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
 
-                        <a href="create_task.php" style="float: right;" class="btn task" id="mgaButtons">Add a Task</a>
+<body>
 
-                        <!-- Table with stripped rows -->
-                        <table class="table datatable">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Description</th>
-                                    <th scope="col">Priority</th>
-                                    <th scope="col">Due Date</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+    <h1 class="text-center mt-4">Update Task Data</h1>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
 
+                <?php
+                if (isset($_GET['id'])) {
+                    $id = $_GET['id'];
+                    $task = "SELECT * FROM `tasks` WHERE `id` = '$id'";
+                    $task_run = mysqli_query($con, $task);
 
-                                <?php
-                                $query = "SELECT * FROM `tasks`";
-                                $query_run = mysqli_query($con, $query);
-                                if (mysqli_num_rows($query_run) > 0) {
-                                    foreach ($query_run as $row) {
-                                        ?>
-                                        <tr>
-                                            <td>
-                                                <?= $row['title']; ?>
-                                            </td>
-                                            <td>
-                                                <?= $row['description']; ?>
-                                            </td>
-                                            <td>
-                                                <?= $row['priority']; ?>
-                                            </td>
-                                            <td>
-                                                <?= $row['due_date']; ?>
-                                            </td>
+                    if (mysqli_num_rows($task_run) > 0) {
+                        foreach ($task_run as $task) {
+                            ?>
 
-                                            <td class="text-center vstack gap-3 mx-auto">
+                            <form action="process.php" method="POST">
 
-                                                <a type="button" class="btn btn-primary" id="customFont-Button"
-                                                    href="index.php?id=<?= $row['id']; ?>">VIEW</a>
-                                                <a type="button" class="btn btn-warning" id="customFont-Button"
-                                                    href="edit_task.php?id=<?= $row['id']; ?>">UPDATE</a>
-                                                <a type="button" class="btn btn-danger" id="customFont-Button"
-                                                    href="delete_task.php?id=<?= $row['id']; ?>">DELETE</a>
+                                <input type="hidden" name="id" value="<?= $task['id']; ?>">
 
-                                            </td>
-                                        </tr>
+                                <div class="row">
+                                    <div class="col-md-12 mb-3 mt-4" id="forForm">
+                                        <label id="forView" for="title" class="form-label">Title</label>
+                                        <h2>
+                                            <?= $task['title']; ?>
+                                        </h2>
+                                    </div>
 
-                                        <?php
-                                    }
-                                } else {
-                                    ?>
-                                    <tr>
-                                        <td colspan="6">No Record Found</td>
-                                    </tr>
-                                    <?php
-                                }
-                                ?>
+                                    <div class="col-md-12 mb-3" id="forForm">
+                                        <label id="forView" for="description" class="form-label">Description</label>
+                                        <h2>
+                                            <?= $task['description']; ?>
+                                        </h2>
+                                    </div>
 
-                            </tbody>
-                        </table>
-                        <!-- End Table with stripped rows -->
-                        <footer>
-                            <p>Task Management System | Regualos & Sarmiento @ 2024</p>
-                        </footer>
+                                    <div class="col-md-4 mb-3 me-4" id="forForm">
+                                        <label id="forView" for="priority" class="form-label">Priority</label>
+                                        <h2>
+                                            <?= $task['priority']; ?>
+                                        </h2>
+                                    </div>
+
+                                    <div class="col-md-4 mb-3" id="forForm">
+                                        <label id="forView" for="dueDate" class="form-label">Due Date</label>
+                                        <h2>
+                                            <?= $task['due_date']; ?>
+                                        </h2>
+                                    </div>
+
+                                    <div class="col-md-12 mb-3 text-center mt-4">
+                                        <button type="button" value="Back" class="btn" onclick="history.back();" id="mgaButtons"
+                                            style="float: left;">Go Back</button>
+                                        <!-- <button type="submit" name="updateTask" class="btn" id="mgaButtons"
+                                            style="float: right;">Submit</button> -->
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
+
+                    <?php
+                        }
+                    } else {
+                        ?>
+                <h4>No Record Found!</h4>
+                <?php
+                    }
+                }
+                ?>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <?php
+    if (isset($_SESSION['status']) && $_SESSION['status_code'] != '') {
+        ?>
+        <script>
+            Swal.fire({
+                position: "center",
+                icon: "<?php echo $_SESSION['status_code']; ?>",
+                title: "<?php echo $_SESSION['status']; ?>",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        </script>
+        <?php
+        unset($_SESSION['status']);
+        unset($_SESSION['status_code']);
+    }
+    ?>
+</body>
+
+</html>
